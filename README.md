5dc20ba30bb4ea1cf54da81f93e405cbc68e7c43
LordGift <jarupath.j@gmail.com>
2024-10-29 22:03:22 +0700

    read temp

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ESP32 - RAIN DETECTOR
 
-This is IoT project focusing on ESP32 module.
+This is IoT project focusing on ESP32 module. It's built-in with..
+- Temperature Sensor
+- Wifi
 
 reference https://wokwi.com/
--- main.c
+++ main.c
@@ -1,10 +1,37 @@
+#ifdef __cplusplus
+  extern "C" {
+#endif
+  uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(10); // this speeds up the simulation
+  delay(1000); 
+
+  readBuiltInTemperature();
+}
+
+void readBuiltInTemperature() {
+  Serial.print("Built-in temp sensor: ");
+  Serial.print(temprature_sens_read());               
+  Serial.print(" F : ");
+
+  Serial.print(fahrenheitToCelsius(temprature_sens_read()));
+  Serial.println(" C");
+}
+
+float fahrenheitToCelsius(uint8_t fahrenheit) {
+    float celsius;
+    celsius = (fahrenheit - 32) / 1.8;
+    return celsius;
 }
