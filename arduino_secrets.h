ba9c1647f71496e70b86ca2d3c44c7598c854260
LordGift <jarupath.j@gmail.com>
2025-01-22 16:53:53 +0700

    cleanup

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,4 @@
 #define SECRET_DEVICE_KEY ""
 #define SECRET_OPTIONAL_PASS ""
 #define SECRET_SSID ""
+#define DEVICE_LOGIN_NAME ""
--- thingProperties.h
+++ thingProperties.h
@@ -3,11 +3,11 @@
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
 
-const char DEVICE_LOGIN_NAME[]  = "aa9e84ec-6175-41cc-bc14-aa7321fa5a71";
+const char DEVICE_LOGIN_NAME[]  = DEVICE_LOGIN_NAME;
 
 const char SSID[]               = SECRET_SSID;    // Network SSID (name)
 const char PASS[]               = SECRET_OPTIONAL_PASS;    // Network password (use for WPA, or use as key for WEP)
-const char DEVICE_KEY[]  = SECRET_DEVICE_KEY;    // Secret device password
+const char DEVICE_KEY[]         = SECRET_DEVICE_KEY;    // Secret device password
 
 CloudContactSensor isRaining;
 CloudTemperatureSensor builtInTemp;
